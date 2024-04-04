372fbc55bad066e02dfd8231bb85f316b9470c7a
Yashas Kashyap <yrk4232@gmail.com>
2024-04-04 13:23:24 +0530

    fdsadsf

--- js-lib-4.js
+++ js-lib-4.js
@@ -448,14 +448,14 @@ const allowedOrigins = [
                 let sessionStorageItems = JSON.parse(sessionStorage.getItem(key));
                 let board_id = null
                 console.log("Yo")
-                for (var item in sessionStorageItems){
-                  console.log(item)
-                    if(item[0]==='boardId'){
-                      console.log(item[0])
-                      console.log(typeof item[0])
-                      board_id = item[1]
-                      break;
-                    }
+                for (var itemKey in sessionStorageItems) {
+                  console.log(itemKey);
+                  if (itemKey.startsWith('boardId')) {
+                    console.log(itemKey);
+                    console.log(typeof itemKey);
+                    board_id = sessionStorageItems[itemKey];
+                    break;
+                  }
                 }
                 values["boardId"] = board_id;
               }
